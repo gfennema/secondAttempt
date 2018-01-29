6f38e79f03bad80b2e61d195bee20db80b30bf5e
Greg Fennema <gfennema@live.com>
2018-01-29 18:30:05 +0000

    added mailer seeting to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMPT_PORT'],
+  :address        => ENV['MAILGUN_SMPT_SERVER'],
+  :user_name      => ENV['MAILGUN_SMPT_LOGIN'],
+  :password       => ENV['MAILGUN_SMPT_PASSWORD'],
+  :domain         => 'pure-island-50602.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
