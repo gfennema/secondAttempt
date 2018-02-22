b59d980dda31583dbebe7d148803633ee5eb9cb7
Greg Fennema <gfennema@live.com>
2018-02-22 19:07:14 +0000

    added bare bones user show page and updated profiles redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     
     # GET to /users/:id
     def show 
-        User.find( params[:id] )
+        @user = User.find( params[:id] )
     end
     
 end
